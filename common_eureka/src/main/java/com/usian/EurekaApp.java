1fe68aa16c2790143ba386fcfda9eb93a8c8b7d7
chenxy <309602374@qq.com>
2021-04-09 11:33:27 +0800

    上课演示修改提交

--- common_eureka/src/main/java/com/usian/EurekaApp.java
+++ common_eureka/src/main/java/com/usian/EurekaApp.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApp{
 
     public static void main(String[] args) {
+        System.out.println("我刚刚增加了内容。。。");
         SpringApplication.run(EurekaApp.class, args);
     }
 }
