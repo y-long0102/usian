d0fc7fa9f0eafaff1481bd0e7def0308627f47ba
ababvv <3309093267@qq.com>
2021-05-19 16:59:59 +0800

    提交到本地

--- usian_sso_web/src/main/java/com/usian/controller/SSOController.java
+++ usian_sso_web/src/main/java/com/usian/controller/SSOController.java
@@ -23,6 +23,7 @@ public class SSOController {
         // 校验通过就是true， 不通过就是false
         Boolean b = ssoFeignService.checkUserInfo(checkValue, checkFlag);
         if(b){
+            System.out.println("哈哈哈哈");
             return Result.ok(b);
         }
         return Result.error("校验失败");
